352534eab2c6b663327e6efffc8d3c5ef4aa8a5c
SallyTemple <pfende@gmail.com>
2018-03-18 07:16:02 -0700

    update

---
+++ assets/javascript/app.js
@@ -0,0 +1,14 @@
+
+
+<script>
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyCPNwSyaGeIqBSCirWVWg_v5I3tUuTWWzY",
+    authDomain: "train-schedule-6eb48.firebaseapp.com",
+    databaseURL: "https://train-schedule-6eb48.firebaseio.com",
+    projectId: "train-schedule-6eb48",
+    storageBucket: "train-schedule-6eb48.appspot.com",
+    messagingSenderId: "699923413540"
+  };
+  firebase.initializeApp(config);
+</script>
