834e4af4d9a99e62f5a2b7148f4b50b34ca2b64f
SallyTemple <pfende@gmail.com>
2018-03-18 08:39:47 -0700

    update

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -8,3 +8,33 @@
     messagingSenderId: "699923413540"
   };
   firebase.initializeApp(config);
+
+  //Create a click function that links to database
+  var database = firebase.database();
+
+  var trainName= "";
+  var destination= "";
+  var firstTrainTime= "";
+  var frequency= "";
+  
+  
+  // get info from form
+  $("#submitButton").on("click", function (event) {
+      // Preventing the buttons default behavior when clicked (which is submitting a form)
+      event.preventDefault();
+      // This line grabs the input from the textbox
+      name = $("#trainName").val().trim();
+      date = $("#destination").val().trim();
+      role = $("firstTrainTime").val().trim();
+      rate = $("#frequency").val().trim();
+  
+      database = usersRef.push({
+  
+          name : trainName,
+          date : destination,
+          role : firstTrainTime,
+          rate : frequency
+  
+      })
+  });
+  
