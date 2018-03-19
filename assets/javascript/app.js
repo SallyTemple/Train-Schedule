62a8907e86cd43b89d2d0ee4c2e1ae8998f1aaaf
SallyTemple <pfende@gmail.com>
2018-03-18 18:28:14 -0700

    update

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,39 +1,59 @@
 
-  var config = {
-    apiKey: "AIzaSyCPNwSyaGeIqBSCirWVWg_v5I3tUuTWWzY",
-    authDomain: "train-schedule-6eb48.firebaseapp.com",
-    databaseURL: "https://train-schedule-6eb48.firebaseio.com",
-    projectId: "train-schedule-6eb48",
-    storageBucket: "train-schedule-6eb48.appspot.com",
-    messagingSenderId: "699923413540"
-  };
-  firebase.initializeApp(config);
+var config = {
+      apiKey: "AIzaSyCPNwSyaGeIqBSCirWVWg_v5I3tUuTWWzY",
+      authDomain: "train-schedule-6eb48.firebaseapp.com",
+      databaseURL: "https://train-schedule-6eb48.firebaseio.com",
+      projectId: "train-schedule-6eb48",
+      storageBucket: "train-schedule-6eb48.appspot.com",
+      messagingSenderId: "699923413540"
+};
+firebase.initializeApp(config);
 
-  //Create a click function that links to database
-  var database = firebase.database();
+//This variable reference the database
+var database = firebase.database();
 
-  var trainName= "";
-  var destination= "";
-  var firstTrainTime= 0;
-  var frequency= 0;
-  
-  
-  // get info from form
-  $("#submitButton").on("click", function(event) {
+
+
+// Submit new train schedule information to the database
+$("#submitButton").on("click", function (event) {
       event.preventDefault();
-      
-      trainName = $("#trainName").val().trim();
-      destination = $("#destination").val().trim();
-      firstTrainTime = $("firstTrainTime").val().trim();
-      frequency = $("#frequency").val().trim();
-  
-     
-  var newTrainSchedule = {
-      trainName : trainName,
-      destination : destination,
-      firstTrainTime: firstTrainTime,
-      frequency : frequency
-  
+
+      // Create variables for the different in the colomns
+      var trainName = "";
+      var destination = "";
+      var firstTrainTime = 0;
+      var frequency = 0;
+
+      //Get the user input
+      trainName = $("#input1").val().trim();
+      destination = $("#input2").val().trim();
+      firstTrainTime = $("input3").val().trim();
+      frequency = $("input4").val().trim();
+
+      //Create variable were the form variable equal the new input
+      var newTrainSchedule = {
+            newTrainName: trainName,
+            newDestination: destination,
+            newFirstTrainTime: firstTrainTime,
+            newFrequency: frequency
       }
+      //Create a function to push the new train schedule into firebase
       database.ref().push(newTrainSchedule);
+
+
+      //Create a function to clear the textbox after each input
+      $("#input1").val("");
+      $("#input2").val("");
+      $("#input3").val("");
+      $("#input4").val("");
+
+      return false;
+});
+
+//Adding a new train schedule in the database
+database.ref().on("child_added", function(childSnapshot, prevChildKey) {
+      var trainName = childSnapshot.val().newTrainName;
+      var  destination = childSnapshot.val().newDestination;
+      var firstTrainTime = childSnapshot.val().newFirstTrainTime;
+      var frequency= childSnapshot.val().newFrequency; 
 });
